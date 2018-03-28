841731fcb070e0cae339389ab0c23d51e9870097
Leonardo Steffen <leosteffen84@gmail.com>
2018-03-28 18:57:00 -0300

    retrieves a list of tasks from tasks/all endpoint

--- routes/tasks.js
+++ routes/tasks.js
@@ -13,14 +13,24 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/all', (req, res, next) => {
-    res.json({
-        id: 1,
-        name: 'sample task',
-        status: 'open',
-        eventId: 11,
-        date: 'jan 12 2019',
-        userId: 1
-    });
+    res.json([
+        {
+            id: 1,
+            name: 'sample task',
+            status: 'open',
+            eventId: 11,
+            date: 'jan 12 2019',
+            userId: 1
+        },
+        {
+            id: 2,
+            name: 'sample task 2',
+            status: 'open',
+            eventId: 11,
+            date: 'jan 12 2019',
+            userId: 1
+        }
+    ]);
 });
 
 module.exports = router;
