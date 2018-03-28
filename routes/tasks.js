8ebd556ee5848002d5538c3cd9e793b948781d9b
Leonardo Steffen <leosteffen84@gmail.com>
2018-03-28 18:53:42 -0300

    adds route to get all tasks; fixes tasks endpoint to render the correct pug view

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,7 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('all available tasks', {
+    res.render('tasks', {
+        id: 1,
+        name: 'sample task',
+        status: 'open',
+        eventId: 11,
+        date: 'jan 12 2019',
+        userId: 1
+    });
+});
+
+router.get('/all', (req, res, next) => {
+    res.json({
         id: 1,
         name: 'sample task',
         status: 'open',
